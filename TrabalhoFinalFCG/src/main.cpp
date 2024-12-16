f5ffb23c2ce004c46f9e44b0614b15688158a3d2
Gabriel Arusiewicz Berta <gabriaru@gmail.com>
2024-12-16 20:14:15 -0300

    #1  Movimentação com eixo y fixo
    parte inicial da movimentação

--- TrabalhoFinalFCG/src/main.cpp
+++ TrabalhoFinalFCG/src/main.cpp
@@ -233,81 +233,49 @@ int main()
     float delta_t = 0;
     glm::vec4 camera_position_new  = glm::vec4(1.0f,1.0f,2.5f,1.0f);
 
-    // Ficamos em um loop infinito, renderizando, até que o usuário feche a janela
+    float g_CameraAlturaFixa = 1.0f; // Altura fixa para a movimentação
+
     while (!glfwWindowShouldClose(window))
     {
-        // Aqui executamos as operações de renderização
-
-        // Definimos a cor do "fundo" do framebuffer como branco.  Tal cor é
-        // definida como coeficientes RGBA: Red, Green, Blue, Alpha; isto é:
-        // Vermelho, Verde, Azul, Alpha (valor de transparência).
-        // Conversaremos sobre sistemas de cores nas aulas de Modelos de Iluminação.
-        //
-        //           R     G     B     A
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-
-        // "Pintamos" todos os pixels do framebuffer com a cor definida acima,
-        // e também resetamos todos os pixels do Z-buffer (depth buffer).
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Pedimos para a GPU utilizar o programa de GPU criado acima (contendo
-        // os shaders de vértice e fragmentos).
         glUseProgram(g_GpuProgramID);
-
-        // "Ligamos" o VAO. Informamos que queremos utilizar os atributos de
-        // vértices apontados pelo VAO criado pela função BuildTriangles(). Veja
-        // comentários detalhados dentro da definição de BuildTriangles().
         glBindVertexArray(vertex_array_object_id);
 
-        // Computamos a posição da câmera utilizando coordenadas esféricas.  As
-        // variáveis g_CameraDistance, g_CameraPhi, e g_CameraTheta são
-        // controladas pelo mouse do usuário. Veja as funções CursorPosCallback()
-        // e ScrollCallback().
+
         float r = g_CameraDistance;
-        float y = r*sin(g_CameraPhi);
-        float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
-        float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
+        float y = r * sin(g_CameraPhi); // Altura ajustada pelo ângulo phi
+        float z = r * cos(g_CameraPhi) * cos(g_CameraTheta);
+        float x = r * cos(g_CameraPhi) * sin(g_CameraTheta);
 
-        // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
-        // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
-        glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
-        glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
-        glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
+        glm::vec4 camera_position_c = glm::vec4(x, g_CameraAlturaFixa, z, 1.0f);
+        glm::vec4 camera_lookat_l = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+        glm::vec4 camera_view_vector = camera_lookat_l - glm::vec4(x, y, z, 1.0f);
+        glm::vec4 camera_up_vector = glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
 
-
-
-        // formula retirada das perguntas frequentes da cadeira
         // Atualiza delta de tempo
         float current_time = (float)glfwGetTime();
         delta_t = current_time - prev_time;
         prev_time = current_time;
 
-
-
-        glm::vec4 w = -camera_view_vector/norm(camera_view_vector);
+        glm::vec4 w = -camera_view_vector / norm(camera_view_vector);
         glm::vec4 u = crossproduct(camera_up_vector, w);
 
-
-        // implementação retirada das perguntas frequetnes da cadeira
-        // Realiza movimentação de objetos
+        // movimentação
         if (tecla_D_pressionada)
-            // Movimenta câmera para direita
             camera_position_new += u * speed * delta_t;
 
         if (tecla_S_pressionada)
-            // Movimenta câmera para tras
             camera_position_new += w * speed * delta_t;
 
         if (tecla_A_pressionada)
-            // Movimenta câmera para esquerda
             camera_position_new += -u * speed * delta_t;
 
         if (tecla_W_pressionada)
-            // Movimenta câmera para frente
             camera_position_new += -w * speed * delta_t;
 
-
+        // Mantém a altura fixa na movimentação
+        camera_position_new.y = g_CameraAlturaFixa;
 
 
         // Computamos a matriz "View" utilizando os parâmetros da câmera para
